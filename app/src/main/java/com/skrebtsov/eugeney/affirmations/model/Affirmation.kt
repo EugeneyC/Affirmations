a9b64f63a0a036fcc55223a395a1dbb464b11a43
EugeneyC <xxtedixx@yandex.ru>
2020-11-12 23:54:52 +0300

    add resource Image

--- app/src/main/java/com/skrebtsov/eugeney/affirmations/model/Affirmation.kt
+++ app/src/main/java/com/skrebtsov/eugeney/affirmations/model/Affirmation.kt
@@ -1,4 +1,7 @@
 package com.skrebtsov.eugeney.affirmations.model
 
-class Affirmation(val stringResourceId: Int) {
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+
+class Affirmation(@StringRes val stringResourceId: Int, @DrawableRes val imageResourceId: Int) {
 }
